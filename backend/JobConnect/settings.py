266aa524fa4e31fe4c06d407a8c545c02e1205cf
smartwa <smartwacaleb@gmail.com>
2025-01-29 12:41:33 +0300

    feat: Add custom icons to Jazzmin settings

--- backend/JobConnect/settings.py
+++ backend/JobConnect/settings.py
@@ -143,39 +143,30 @@ AUTH_USER_MODEL = "users.CustomUser"
 JAZZMIN_SETTINGS = {
     "show_ui_builder": True,
     "site_title": "JobConnect",
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_header": "JobConnect",
     "site_brand": "JobConnect",
-    # Logo to use for your site, must be present in static files, used for brand on top left
     "site_logo": "default/logo.png",
-    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    # "login_logo": None,
-    # Logo to use for login form in dark themes (defaults to login_logo)
-    # "login_logo_dark": None,
-    # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    # "site_icon": None,
-    # Welcome text on the login screen
     "welcome_sign": "Welcome to JobConnect",
-    # Copyright on the footer
     "copyright": "JobConnect",
-    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": "profile",
     "topmenu_links": [
-        # Url that gets reversed (Permissions can be added)
         {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
-        # external url that opens in a new window (Permissions can be added)
         {
             "name": "Support",
             "url": "https://github.com/Simatwa/JobConnect/issues",
             "new_window": True,
         },
-        # model admin to link to (Permissions checked against model)
         {"model": "auth.User"},
-        # App with dropdown menu to all its models pages (Permissions checked against models)
         {"app": "jobs"},
     ],
     "search_model": ["jobs.Job"],
     "language_chooser": True,
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "users.CustomUser": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "jobs.Job": "fas fa-briefcase",
+        "jobs.JobCategory": "fas fa-list",
+    },
 }
